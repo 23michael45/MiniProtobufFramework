a13556f59629570dc6859d8879c30dba5d86e79d
23michael45 <michael2345@live.cn>
2018-11-29 10:17:43 +0800

    add http main function

--- MiniProtobufServer/main.cpp
+++ MiniProtobufServer/main.cpp
@@ -1,13 +1,13 @@
 ﻿#include "TcpServer.h"
 
+#include "HttpServer/http_server.hpp"
 
-int main()
+void TcpServerStart()
 {
-
 	try
 	{
 		asio::io_context io_context;
-		std::shared_ptr<TcpServer> sp_server =  std::make_shared<TcpServer>(io_context);
+		std::shared_ptr<TcpServer> sp_server = std::make_shared<TcpServer>(io_context);
 		sp_server->start();
 		io_context.run();
 	}
@@ -15,5 +15,33 @@ int main()
 	{
 		std::cerr << e.what() << std::endl;
 	}
+}
+void HttpServerStart()
+{
+	try
+	{
+		std::string  address = "127.0.0.1";
+		std::string  port = "89";
+		std::string  docroot = "D:/DevelopProj/ProtocolBuffers/MiniProtobufFramework/MiniProtobufFramework/MiniProtobufServer/HttpServer/WebSiteRoot";
+
+		// Initialise the server.
+		http::server::server s(address, port, docroot);
+
+		// Run the server until stopped.
+		s.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "exception: " << e.what() << "\n";
+	}
+}
+
+
+int main()
+{
+	//HttpServerStart();
+	TcpServerStart();
+
+
 	return 0;
 }
